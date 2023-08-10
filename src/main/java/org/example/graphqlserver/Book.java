ec6a9bacbac500f11862505bfe17e9058ca99547
SoeSuNwe <ssnwe@galaxywave.com>
2023-08-10 14:22:14 +0630

    update rescursive filter list

--- src/main/java/org/example/graphqlserver/Book.java
+++ src/main/java/org/example/graphqlserver/Book.java
@@ -35,71 +35,39 @@ public class Book {
         return books.stream().filter(book -> book.getId().equals("1")).findFirst().orElse(null);
     }
 
-    public static List<Book> filterBooks(List<Where> filters) {
-        System.out.println("************************");
-        for (var i = 0; i < filters.size(); i++) {
-            recursiveFilter(filters, i);
-            System.out.println("************************");
+    public static List<Book> filterBooks(List<Where> whereList) {
+        System.out.println("**************************filterBooks****** size " + whereList.size());
+        for (var i = 0; i < whereList.size(); i++) {
+            System.out.println("**************************filterBooks****** index " + i);
+            recursiveWhere(whereList.get(i), i);
         }
         return books;
     }
 
-    public static void recursiveFilter(List<Where> whereList, int head) {
-        if (whereList.size() > head) {
-            System.out.println("filter=>[" + head + "] \t" + whereList.get(head).getFilter().getAttributeId() + " " + whereList.get(head).getFilter().getOperator()
-                    + " " + whereList.get(head).getFilter().getAttributeValue() + "\t");
 
-            if ((whereList.get(head).getAnd() != null) && (whereList.get(head).getAnd().size() > 0)) {
-                for (var i = 0; i < whereList.get(head).getAnd().size(); i++) {
-                    System.out.printf("And[" + head + "]");
-                    recursiveFilter(whereList.get(head).getAnd(), i);
-                }
-            }
-            if ((whereList.get(head).getOr() != null) && whereList.get(head).getOr().size() > 0) {
-                for (var i = 0; i < whereList.get(head).getOr().size(); i++) {
-                    System.out.printf(" OR[" + head + "]");
-                    recursiveFilter(whereList.get(head).getOr(), i);
-                }
-            }
-            if (whereList.get(head).getNot() != null) {
-                System.out.println("Not[" + head + "]filter=>[" + head + "]\t" + whereList.get(head).getNot().getFilter().getAttributeId() + " " + whereList.get(head).getNot().getFilter().getOperator()
-                        + " " + whereList.get(head).getNot().getFilter().getAttributeValue() + "\t");
-                if ((whereList.get(head).getNot().getAnd() != null) && whereList.get(head).getNot().getAnd().size() > 0) {
-                    for (var i = 0; i < whereList.get(head).getNot().getAnd().size(); i++) {
-                        System.out.printf("Not[" + head + "] And=>");
-                        recursiveFilter(whereList.get(head).getNot().getAnd(), i);
-                    }
-                }
-                if ((whereList.get(head).getNot().getOr() != null) && whereList.get(head).getNot().getOr().size() > 0) {
-                    for (var i = 0; i < whereList.get(head).getNot().getOr().size(); i++) {
-                        System.out.printf("Not[" + head + "] OR=>");
-                        recursiveFilter(whereList.get(head).getNot().getOr(), i);
-                    }
-                }
-                if (whereList.get(head).getNot().getNot() != null) {
-                    System.out.println("Not[" + head + "]filter=>[" + head + "]\t" + whereList.get(head).getNot().getNot().getFilter().getAttributeId() +
-                            " " + whereList.get(head).getNot().getNot().getFilter().getOperator()
-                            + " " + whereList.get(head).getNot().getNot().getFilter().getAttributeValue() + "\t");
-                    if ((whereList.get(head).getNot().getNot().getAnd() != null) && whereList.get(head).getNot().getNot().getAnd().size() > 0) {
-                        for (var i = 0; i < whereList.get(head).getNot().getNot().getAnd().size(); i++) {
-                            System.out.printf("Not[" + head + "] And=>");
-                            recursiveFilter(whereList.get(head).getNot().getNot().getAnd(), i);
-                        }
-                    }
-                    if ((whereList.get(head).getNot().getNot().getOr() != null) && whereList.get(head).getNot().getNot().getOr().size() > 0) {
-                        for (var i = 0; i < whereList.get(head).getNot().getNot().getOr().size(); i++) {
-                            System.out.printf("Not[" + head + "] OR=>");
-                            recursiveFilter(whereList.get(head).getNot().getNot().getOr(), i);
-                        }
-                    }
-                }
-            }
-
-        } else {
-            recursiveFilter(whereList, head + 1);
-        }
+    public static void filterOutput(Filter filter, int i) {
+        System.out.println("filter=>" + i + "=>" + filter.getAttributeId() + " " + filter.getOperator()
+                + " " + filter.getAttributeValue() + "\t");
     }
 
+    public static void recursiveWhere(Where where, int i) {
+        if (where.getFilter() != null) {
+            filterOutput(where.getFilter(), i);
+        }
+        if (where.getAnd() != null) {
+            for (Where w : where.getAnd()) {
+                recursiveWhere(w, i);
+            }
+        }
+        if (where.getOr() != null) {
+            for (Where w : where.getOr()) {
+                recursiveWhere(w, i);
+            }
+        }
+        if (where.getNot() != null) {
+            recursiveWhere(where.getNot(), i);
+        }
+    }
 
     public String getId() {
         return id;
